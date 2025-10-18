31fd806fe8f8f08e20bd499139d15cc148674877
Laureano Diez <contactolaureanodiez@gmail.com>
2025-10-18 09:38:28 -0300

    Astar con heurística distancia Manhattan

--- tp-pathfinding/src/pathfinder/search/astar.py
+++ tp-pathfinding/src/pathfinder/search/astar.py
@@ -18,12 +18,55 @@ class AStarSearch:
         # Initialize root node
         root = Node("", state=grid.initial, cost=0, parent=None, action=None)
 
+        # Inicializar la heurística con el estado inicial y el objetivo final
+        h = AStarSearch.h(root.state, grid.end)
+        frontera = PriorityQueueFrontier()
+        # Añadir el costo de la heurística
+        frontera.add(root, root.cost+h)
         # Initialize reached with the initial state
         reached = {}
         reached[root.state] = root.cost
+        while True:
+                # Si la frontera está vacía, no hay solución
+                if frontera.is_empty():
+                    return NoSolution(reached)
 
-        # Initialize frontier with the root node
-        # TODO Complete the rest!!
-        # ...
+                #  Extraer el nodo con menor costo acumulado
+                nodo = frontera.pop()
+
+                #  Verificar si este nodo alcanza el objetivo
+                if grid.objective_test(nodo.state):
+                    return Solution(nodo, reached)
+
+                #  Expandir los sucesores
+                for accion in grid.actions(nodo.state):
+                    sucesor = grid.result(nodo.state, accion)
+                    nuevo_costo = nodo.cost + grid.individual_cost(nodo.state, accion)
+
+                    #  Si no fue alcanzado o encontramos un costo menor
+                    if sucesor not in reached or nuevo_costo < reached[sucesor]:
+                        # Crear nuevo nodo hijo
+                        Nhijo = Node("", sucesor, nuevo_costo, nodo, accion)
+
+                        # Actualizar costo alcanzado
+                        reached[sucesor] = nuevo_costo
+                        # Actualizar la heurística
+                        h_act = AStarSearch.h(sucesor, grid.end)
+
+                        # Añadir a la frontera contemplando la heurística actualizada
+                        frontera.add(Nhijo, nuevo_costo+h_act)    
 
         return NoSolution(reached)
+    
+    def h(state, end)->int:
+        """Implementación de Heurística: Calcula la distancia Manhattan entre dos puntos
+        
+        Args:
+            state: Estado actual (tupla con coordenadas x, y)
+            end: Estado objetivo (tupla con coordenadas x, y)
+            
+        Returns:
+            int: Distancia Manhattan
+        """
+        return abs(state[0] - end[0]) + abs(state[1] - end[1])
+        
